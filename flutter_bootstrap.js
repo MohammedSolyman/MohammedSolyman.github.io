3db0490072987184068e696ebe9501e2f2279e89
MohammedSolyman <MohammedSolyman10@gmail.com>
2025-01-05 19:47:45 +0200

    f6

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2775510472"
+    serviceWorkerVersion: "3252767055"
   }
 });
